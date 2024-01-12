998f03adfcb960b096b5302db36eafe788fe3928
ZulicS <sanel.zulic@hmhco.com>
2024-01-12 16:05:01 +0100

    Adding chatbot

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,53 @@
+import { BubbleChat } from 'flowise-embed-react';
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="absolute z-30 w-full">
+      <BubbleChat
+        chatflowid="050de8c0-ee9c-48ba-9372-2def283489c3"
+        apiHost="http://16.170.241.71"
+        theme={{
+          button: {
+            backgroundColor: '#3B81F6',
+            right: 20,
+            bottom: 20,
+            size: 'medium',
+            iconColor: 'white',
+            customIconSrc:
+              'https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg',
+          },
+          chatWindow: {
+            welcomeMessage: 'Hello! This is custom welcome message',
+            backgroundColor: '#ffffff',
+            height: 700,
+            width: 400,
+            fontSize: 16,
+            poweredByTextColor: '#303235',
+            botMessage: {
+              backgroundColor: '#f7f8ff',
+              textColor: '#303235',
+              showAvatar: true,
+              avatarSrc:
+                'https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/parroticon.png',
+            },
+            userMessage: {
+              backgroundColor: '#3B81F6',
+              textColor: '#ffffff',
+              showAvatar: true,
+              avatarSrc:
+                'https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png',
+            },
+            textInput: {
+              placeholder: 'Type your question',
+              backgroundColor: '#ffffff',
+              textColor: '#303235',
+              sendButtonColor: '#3B81F6',
+            },
+          },
+        }}
+      />
+    </header>
+  );
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,62 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="flex flex-col justify-center text-center xl:pt-40 xl:text-left h-full container mx-auto">
+        <div className="flex flex-col justify-center text-center xl:pt-40 xl:text-left h-full container mx-auto z-10 relative font-sora">
           {/* title */}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn('down', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1 max-w-sm xl:max-w-xl mx-auto xl:mx-0 xl:mb-8"
+          >
             Transforming Ideas into{' '}
             <span className="text-accent">Digital Reality</span>
-          </h1>
-          <p>
+          </motion.h1>
+          <motion.p
+            variants={fadeIn('down', 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl  mx-auto xl:mx-0 mb-10 xl:mb-8"
+          >
             I&apos;m a full stack developer with a passion for building high
             quality software. I specialize in building exceptional digital
             experiences and everything in between.
-          </p>
+          </motion.p>
           {/* btn */}
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
           </div>
+          <motion.div
+            variants={fadeIn('down', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}
-      <div>image</div>
+      <div className="w-[1200px] h-full absolute bottom-0 right-0">
+        {/* bg-image */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* particles */}
+        <ParticlesContainer />
+        {/* avatar image */}
+        <motion.div
+          variants={fadeIn('up', 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1.5, ease: 'easeInOut' }}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
+          <Avatar />
+        </motion.div>
+      </div>
     </div>
   );
 };
